dcafd23938adddd47d5265d43d66c9fdc98fae53
Teo Petrea <53831298+teopufulete@users.noreply.github.com>
2020-05-21 12:43:27 +0300

    test implementation for rock paper scissors
    To do: implement prisoners dilemma and battle of the sexes games

---
+++ test_nonzero.py
@@ -0,0 +1,60 @@
+import numpy as np
+from non_zero_sum_game import NonZeroSumGame
+
+SEED = 1337
+
+def simulate_game(get_score, action_1, action_2, iters=10 ** 4, comments = None, verbosity = 1):
+    game = NonZeroSumGame(get_score, action_1, action_2)
+
+    score_1, score_2 = 0, 0
+    for it in range(iters):
+        act_1 = game.generate_action(1)
+        act_2 = game.generate_action(2)
+        f_score, s_score = get_score(act_1, act_2)
+        score_1 += f_score
+        score_2 += s_score
+
+    if verbosity >= 1:
+        if comments is not None:
+            print(comments)
+        print(game.nash_eq[0])
+        print(f'Game price first: {game.game_cost[0]}')
+        print(f'Average score first: {score_1 / iters}')
+        print()
+        print(game.nash_eq[1])
+        print(f'Game price second: {game.game_cost[1]}')
+        print(f'Average score second: {score_2 / iters}')
+        print('-' * 80)
+    return game.nash_eq, game.game_cost
+
+
+def rock_paper_scissors(first_act, second_act):
+    FIGHTS = {('rock', 'scissors'),
+                ('paper', 'rock'),
+                ('scissors', 'paper')}
+    ACTIONS = ('rock', 'paper', 'scissors')
+
+    assert first_act in ACTIONS and second_act in ACTIONS
+    if (first_act, second_act) in FIGHTS:
+        return 1, -1
+    if (second_act, first_act) in FIGHTS:
+        return -1, 1
+    return 0, 0
+
+
+if __name__ == '__main__':
+    np.random.seed(SEED)
+    simulate_game(rock_paper_scissors,
+                  ['rock', 'paper', 'scissors'],
+                  ['rock', 'paper', 'scissors'],
+                  comments="Fair rock paper scissors with zero price")
+
+    simulate_game(rock_paper_scissors,
+                  ['rock', 'paper', 'scissors'],
+                  ['rock', 'paper'],
+                  comments="Unfair rock paper scissors where the second player can't use 'scissors'")
+
+    simulate_game(rock_paper_scissors,
+                  ['rock', 'paper', 'scissors'],
+                  ['rock'],
+                  comments="Unfair rock paper scissors where the second player can't use 'scissors' and 'paper'")
